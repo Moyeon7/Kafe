d620cc1c6133e129d92ef69a54e242d9bca127d8
Sakshi Chavan <sakshichavan3204@gmail.com>
2024-07-27 21:12:14 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View className='bg-black w-full h-full'>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
