a593077500f9006d41ab444775827e9f880106b1
Sakshi Chavan <sakshichavan3204@gmail.com>
2024-07-28 12:55:07 +0530

    added customized colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#d3ad7f",
+        black: "#13131a",
+        bg: "#010103",
+        darkgray: "1a1f25",
+        lightgray: "#272c35",
+      },
+    },
   },
   plugins: [],
 };
